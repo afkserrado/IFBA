339e17f0191185af76957c81464599e683d457b2
afkserrado <akserrado@gmail.com>
2025-06-29 19:25:40 -0300

    Att

--- INF029-AndersonSerrado-Codes/AndersonSerrado-20242160026-T2/main.c
+++ INF029-AndersonSerrado-Codes/AndersonSerrado-20242160026-T2/main.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include "EstruturaVetores.h"
 
-int menu();
+/*int menu();
 
-void dobrar(int *x);
+void dobrar(int *x);*/
 
-int menu()
-{
+int menu() {
     int op;
     printf("Digite as opção desejada\n");
     printf("0 - Sair\n");
@@ -20,100 +19,110 @@ int menu()
     return op;
 }
 
-int main()
-{
-    inicializar();
+int main() {
+
+    //inicializar();
     int op;
     int sair = 0;
     int ret;
-    while (!sair)
-    {
+
+    while (!sair) {
+        
         op = menu();
-        switch (op)
-        {
-        case 0:
-        {
-            sair = 1;
-            finalizar();
-            break;
-        }
-        case 1:
-        { //inserir
+        switch (op) {
+        
+            // Sai do programa e libera a memória alocada
+            case 0: {
+                sair = 1;
+                //finalizar();
+                break;
+
+                // Obs.: lembrar que se não tiver memória alocada, não pode dar free
+            }
+
+            // Inserir
+            case 1: {
+                
+                // Insere um número na estrutura, passando a posição e o valor
+                ret = inserirNumeroEmEstrutura(5, 25);
+                
+                if (ret == POSICAO_INVALIDA) {
+                    printf("Posição inválida");
+                }
+ 
+                else if (ret == SUCESSO) {
+                    printf("Inserido com sucesso");
+                }
+
+                else if (ret == SEM_ESPACO) {
+                    printf("Sem Espaço");
+                }
+
+                else if (ret == SEM_ESTRUTURA_AUXILIAR) {
+                    printf("Sem estrutura Auxiliar");
+                }
+                break;
+            }
+
+            /*
+            //excluir
             //TODO
-            ret = inserirNumeroEmEstrutura(5, 25);
-            if (ret == SUCESSO)
-            {
-                printf("Inserido com sucesso");
+            case 2: {
+            
+                break;
             }
-            else if (ret == SEM_ESPACO)
-            {
-                printf("Sem Espaço");
-            }
-            else if (ret == SEM_ESTRUTURA_AUXILIAR)
-            {
-                printf("Sem estrutura Auxiliar");
-            }
-            break;
-        }
 
-        case 2:
-        { //excluir
-            //TODO
-            break;
-        }
+            case 3: {
+            //recuperar dados estrutura auxiliar
+                int posicao, retorno;
+                printf("Qual a estrutura a ser listada (1..10)?");
+                scanf("%d", &posicao);
 
-        case 3:
-        { //recuperar dados estrutura auxiliar
-            int posicao, retorno;
-            printf("Qual a estrutura a ser listada (1..10)?");
-            scanf("%d", &posicao);
+                int qtd = getQuantidadeElementosEstruturaAuxiliar(posicao);
 
-            int qtd = getQuantidadeElementosEstruturaAuxiliar(posicao);
+                if (qtd == POSICAO_INVALIDA) {
+                
+                    printf("Posição inválida");
+                }
 
-            if (qtd == POSICAO_INVALIDA)
-            {
-                printf("Posição inválida");
-            }
-            else
-            { // existe elemento
-                int vetorAux[qtd];
+                else {
+                // existe elemento
+                    int vetorAux[qtd];
 
-                retorno = getDadosEstruturaAuxiliar(posicao, vetorAux);
+                    retorno = getDadosEstruturaAuxiliar(posicao, vetorAux);
 
-                if (retorno == SUCESSO)
-                {
-                    //imprimir para os dados para o usuário
-                    int i = 0;
-                    for (; i < qtd; i++)
-                    {
-                        printf("%d", vetorAux[i]);
+                    if (retorno == SUCESSO) {
+                    
+                        //imprimir para os dados para o usuário
+                        int i = 0;
+                        for (; i < qtd; i++) {
+                        
+                            printf("%d", vetorAux[i]);
+                        }
                     }
                 }
+                break;
             }
-            break;
-        }
 
-        case 10:
-        { //dobrar
-            //ler um numero
-            int valor;
-            scanf("%i", &valor);
+            case 10: {
+            //dobrar
+                //ler um numero
+                int valor;
+                scanf("%i", &valor);
 
-            dobrar(&valor);
+                dobrar(&valor);
 
-            //passar para um funcao (void dobrar(...)) que recebe o numero e dobra (EstruturaVetores.c)
+                //passar para um funcao (void dobrar(...)) que recebe o numero e dobra (EstruturaVetores.c)
 
-            printf("%i", valor);
+                printf("%i", valor);
 
-            break;
-        }
+                break;
+            }*/
 
-        default:
-        {
-            printf("opcao inválida\n");
-        }
+            default: {
+                printf("Opção inválida\n");
+            }
         }
     }
-
     return 0;
 }
