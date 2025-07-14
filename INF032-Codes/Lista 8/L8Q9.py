1f0db8bb7e4583795c11924796c30d35251f30ea
Anderson Serrado <akserrado@gmail.com>
2025-07-14 16:48:14 -0300

    L8Q9

---
+++ INF032-Codes/Lista 8/L8Q9.py
@@ -0,0 +1,41 @@
+'''
+9. Faça um programa que receba como entrada o nome de um arquivo de entrada e outro de saída. O arquivo de entrada contém em cada linha o nome de uma cidade (ocupando 40 caracteres) e o seu numero de habitantes. O programa deverá ler o arquivo de entrada e gerar um arquivo de saída onde aparece o nome da cidade mais populosa seguida pelo seu numero de habitantes.
+'''
+
+# Obtém os caminhos
+entrada = input("Informe o caminho de entrada, incluindo nome do arquivo: ")
+saida = input("Informe o caminho de saída, incluindo nome do arquivo: ")
+
+# Ajustando caminhos
+entrada = entrada.replace("\\", "/")
+saida = saida.replace("\\", "/")
+
+try: 
+    # Abrindo arquivos
+    arqEntrada = open(entrada, "r", encoding="utf-8")
+    arqSaida = open(saida, "w", encoding="utf-8")
+
+    dadosEntrada = arqEntrada.readlines()
+    
+    # Cria um dicionário com os dados
+    cidades = {}
+    for linha in dadosEntrada:
+        cidades[linha[:40]] = int(linha[40:].strip().replace(".",''))
+
+    # Obtém a população máxima
+    popMax = max(cidades.values())
+
+    for chave, valor in cidades.items():
+        if valor == popMax:
+            popFormatada = f"{popMax:,}".replace(",",".")
+            resultado = chave + popFormatada + "\n"
+            arqSaida.write(resultado)
+            break
+
+    # Fechando arquivos
+    arqEntrada.close()
+    arqSaida.close()
+    
+# Erro ao ler arquivo ou arquivo não encontrado
+except (OSError):
+    print("Erro ao ler arquivo.")
