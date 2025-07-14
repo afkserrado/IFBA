3879fe6d85fe080b75dbd65b90e59f896688a54a
Anderson Serrado <akserrado@gmail.com>
2025-07-13 21:01:54 -0300

    Início da lista 8

--- INF032-Codes/Teste.py
+++ INF032-Codes/Teste.py
@@ -1,13 +1,2 @@
 import matplotlib.pyplot as plt
 import numpy as ny
-
-x = ny.arange(1, 20) # Cria um vetor x
-y = 3 * x - 3 # Cria um vetor y
-
-plt.plot(x, y)  # Cria um gráfico
-plt.title("Reta") # Título do gráfico
-plt.xlabel("Eixo x") # Título do eixo x
-plt.ylabel("Eixo y") # Título do eixo y
-plt.show() # Exibe o gráfico
-
-# Teste
