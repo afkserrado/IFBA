d5fec5db3509e14a869863e48edef9bc05376508
afkserrado <akserrado@gmail.com>
2025-07-24 21:36:01 -0300

    Att

--- INF006-Codes/T-L3/L2Q2.c
+++ INF006-Codes/T-L3/L2Q2.c
@@ -1,7 +1,8 @@
+/*
 2) Considere o exemplo abaixo de rotação simples a direita, apresente um trecho de código que 
 dado o nó com FB=-2 efetue uma rotação simples a direita deste nó.
+*/
 
-```
 void rot_dir (arvore *arv, no *v) {
     if (v == NULL) {return;}
 
@@ -30,4 +31,3 @@ void rot_dir (arvore *arv, no *v) {
         mae_v->dir = u;
     }
 }
-´´´
