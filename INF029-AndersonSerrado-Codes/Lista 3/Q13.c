6039653dca60cb270d8de5446d38b009d3913525
afkserrado <akserrado@gmail.com>
2025-08-02 19:59:05 -0300

    Att

--- INF029-AndersonSerrado-Codes/Lista 3/Q13.c
+++ INF029-AndersonSerrado-Codes/Lista 3/Q13.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 /*
-Lista de exercícios - Recursão
 13. Faça uma função recursiva que receba um número inteiro positivo N e imprima todos os números natruais de 0 até N em ordem decrescente.
 */
 
@@ -10,6 +9,7 @@ void impDec (int N) {
 	if (N < 0) {
 		return;
 	}
+	
 	printf("N = %d\n", N);
 	if (N > 0) {impDec(N - 1);}
 }
