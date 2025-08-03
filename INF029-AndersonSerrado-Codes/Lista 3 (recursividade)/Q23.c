0825e4199e41fabc71cd69ae2af77d543ae6c873
afkserrado <akserrado@gmail.com>
2025-08-03 00:24:47 -0300

    Mudança de nome da pasta

---
+++ INF029-AndersonSerrado-Codes/Lista 3 (recursividade)/Q23.c
@@ -0,0 +1,23 @@
+/*
+23) A sequência de Padovan é uma sequência de naturais P(n) definida pelos valores iniciais
+P(0) = P(1) = p(2) = 1
+e a seguinte relação recursiva
+P(n) = P(n - 2) + P(n - 3) se n > 2
+Alguns valores da sequência são: 1, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12, 16, 21, 28...
+Faça uma função recursiva que receba um número N e retorne o N-ésimo termo da
+sequência de Padovan.
+*/
+
+#include <stdio.h>
+
+int padovan(int n) {
+    
+}
+
+int main () {
+    int n = 10;
+    int r = padovan(n);
+    printf("r = %d\n", r);
+
+    return 0;
+}
