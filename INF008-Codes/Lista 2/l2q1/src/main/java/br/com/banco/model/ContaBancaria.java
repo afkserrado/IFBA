7ac83ed28401909f19e3c4261a80767baaea3edb
afkserrado <akserrado@gmail.com>
2025-10-14 00:26:25 -0300

    Att

---
+++ INF008-Codes/Lista 2/l2q1/src/main/java/br/com/banco/model/ContaBancaria.java
@@ -0,0 +1,37 @@
+package br.com.banco.model;
+
+public class ContaBancaria {
+    private String numero;
+    private double saldo;
+    private String tipo; // "CORRENTE" ou "POUPANCA"
+
+    // Construtor 1
+    public ContaBancaria(String numero, String tipo) {
+        this.numero = numero;
+        this.tipo = tipo;
+        this.saldo = 0.0;
+    }
+
+    // Construtor 2
+    public ContaBancaria(String numero, String tipo, double saldoInicial) {
+        // TODO: Implementar chamada a outro construtor
+        // TODO: Validar saldoInicial >= 0
+        this(numero, tipo); // Deve ser a primeira linha do construtor
+        if (saldoInicial >= 0) {
+            saldo = saldoInicial; // 'this' não é obrigatório aqui            
+        }
+        else {
+            saldo = 0;
+        }
+    }
+
+    // TODO: Criar métodos sobrecarregados para depositar
+    // depositar(double valor) e depositar(double valor, String descricao)
+    private boolean validarValor(double valor) {
+        return valor > 0;
+    }
+
+    
+
+    // TODO: Criar getters APENAS para os atributos necessários
+}
