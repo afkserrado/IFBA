fcc4f2cb3cbf80ff59a3a41cd28ff0ae14824367
afkserrado <akserrado@gmail.com>
2025-10-25 01:53:58 -0300

    Att

--- INF008-Codes/Lista 1 (Fred)/C++, Js e Py/l1fq6/country.py
+++ INF008-Codes/Lista 1 (Fred)/C++, Js e Py/l1fq6/country.py
@@ -4,15 +4,10 @@
 Além disso, cada país mantém uma lista de outros países com os quais ele faz fronteira. Escreva a classe em Java e forneça os seus membros a seguir:
 
 a) Construtor que inicialize o código ISO, o nome e a dimensão do país;
-
 b) Métodos de acesso (getter/setter) para as propriedades código ISO, nome, população e dimensão do país;
-
 c) Um método que permita verificar se dois objetos representam o mesmo país (igualdade semântica). Dois países são iguais se tiverem o mesmo código ISO;
-
 d) Um método que informe se outro país é limítrofe do país que recebeu a mensagem;
-
 e) Um método que retorne a densidade populacional do país;
-
 f) Um método que receba um país como parâmetro e retorne a lista de vizinhos comuns aos dois países.
 
 Considere que um país tem no máximo 40 outros países com os quais ele faz fronteira.
