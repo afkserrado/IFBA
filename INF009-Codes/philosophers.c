53511209249a5d65655c9f005ae8451ade857800
afkserrado <akserrado@gmail.com>
2025-11-02 00:27:59 -0300

    Att

--- INF009-Codes/philosophers.c
+++ INF009-Codes/philosophers.c
@@ -47,6 +47,8 @@ int pthread_join(pthread_t thread, void **retval);
 // General
 #define TRUE 1
 #define N 5     // Number of philosophers
+#define UNBLOCKED 1
+#define BLOCKED 0
 
 // Philosophers' states
 #define THINKING 0      // Philosopher is thinking          
@@ -72,6 +74,16 @@ void test(int i);             // Checks whether philosopher i can start eating
 void think(int i);             // Simulates the thinking process
 void eat(int i);               // Simulates the eating process
 
+void chageState(int i, char* state) {
+    printf("Change of state: philosopher %d is now %s\n", i, state);
+    fflush(stdout);
+}
+
+void action(int i, char* action) {
+    printf("Philosopher %d is %s\n", i, action);
+    fflush(stdout);
+}
+
 // Philosopher thread function
 void* philosopher(void* arg) {
     int i = *(int*)arg; // Convert arg to integer pointer and get philosopher ID
@@ -88,21 +100,54 @@ void* philosopher(void* arg) {
 
 // Take the forks
 void take_forks(int i) {
+    
+    int value;
+    sem_getvalue(&mutex, &value);
+
+    if(value == 0) {
+        action(i, "BLOCKED (MUTEX, TAKE_FORKS)");
+    }
+    
     sem_wait(&mutex);   // Try to enter the critical region or block the philosopher if another process is already in it
+
+    action(i, "IN THE CRITICAL REGION (TAKE_FORKS)");
+
     state[i] = HUNGRY;  // Change philosopher's state
-    printf("Change of state: philosopher %d is now HUNGRY\n", i);
+    chageState(i, "HUNGRY");
     test(i);            // Try to acquire both forks
+
+    action(i, "OUT THE CRITICAL REGION (TAKE_FORKS)");
+
     sem_post(&mutex);   // Leave the critical region and wake up a process if there is one blocked waiting to enter the CR
+
+    if(state[i] != EATING) {
+        action(i, "BLOCKED (FORKS, TAKE_FORKS)");
+    }
+
     sem_wait(&s[i]);    // Block the philosopher if forks were not acquired
 }
 
 // Return the forks
 void put_forks(int i) {
+    
+    int value;
+    sem_getvalue(&mutex, &value);
+
+    if(value == 0) {
+        action(i, "BLOCKED (MUTEX, PUT_FORKS)");
+    }
+    
     sem_wait(&mutex);       // Try to enter the critical region or block the philosopher if another process is already in it
+    
+    action(i, "IN THE CRITICAL REGION (PUT_FORKS)");
+
     state[i] = THINKING;    // Change philosopher's state
-    printf("Change of state: philosopher %d is now THINKING\n", i);
+    chageState(i, "THINKING");
     test(LEFT(i));          // Try to wake-up the left neighbor
     test(RIGHT(i));         // Try to wake-up the right neighbor
+
+    action(i, "OUT THE CRITICAL REGION (PUT_FORKS)");
+    
     sem_post(&mutex);       // Leave the critical region and wake up a process if there is one blocked waiting to enter the CR
 }
 
@@ -110,21 +155,21 @@ void put_forks(int i) {
 void test(int i) {
     if(state[i] == HUNGRY && state[LEFT(i)] != EATING && state[RIGHT(i)] != EATING) {
         state[i] = EATING;  // Philosopher took the forks and is eating
-        printf("Change of state: philosopher %d is now EATING\n", i);
+        chageState(i, "EATING");
         sem_post(&s[i]);    // In take_forks, avoid to block a philosopher. In put_forks, wake-up the blocked neighbor
     }
 }
 
 // Simulate eating
 void eat(int i) {
-    printf("Philosopher %d is EATING\n", i);
+    action(i, "EATING");
     fflush(stdout);  // Forces the print immediately
     usleep(1000000); // Simulate eating time (1 s)
 }
 
 // Simulate thinking
 void think(int i) {
-    printf("Philosopher %d is THINKING\n", i);
+    action(i, "THINKING");
     fflush(stdout); // Forces the print immediately
     usleep(500000); // Simulate thinking time (0.5 s)
 }
