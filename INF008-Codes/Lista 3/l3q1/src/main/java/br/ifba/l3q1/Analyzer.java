f60b0e901db61675b4f0ea636efddce0d054a580
afkserrado <akserrado@gmail.com>
2025-11-20 21:36:53 -0300

    Implementação de Analyzer e das classes que a implementam

--- INF008-Codes/Lista 3/l3q1/src/main/java/br/ifba/l3q1/Analyzer.java
+++ INF008-Codes/Lista 3/l3q1/src/main/java/br/ifba/l3q1/Analyzer.java
@@ -1,7 +1,9 @@
 package br.ifba.l3q1;
 
-interface Analyzer<T> {
-    // Public and static methods (default)
-    
+// 'D' is any subclass of Data
+interface Analyzer<D extends Data<?>> {
+    // Public and static methods by default in interfaces
 
+    // Receives a D (a Data subclass) and returns a D
+    D analyze(D data);
 }
---
+++ INF008-Codes/Lista 3/l3q1/src/main/java/br/ifba/l3q1/StatisticalAnalyzer.java
@@ -0,0 +1,11 @@
+package br.ifba.l3q1;
+
+public class StatisticalAnalyzer<D extends Data<?>> implements Analyzer<D> {
+    
+    // Implements 'analyze'
+    @Override
+    public D analyze(D data) {
+        // ML regression logic
+        return data;
+    }
+}
