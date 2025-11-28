0ae8674f4f8d0aefb52b59e1ec75ee1fb8a92bf0
afkserrado <akserrado@gmail.com>
2025-11-27 23:46:07 -0300

    Att

--- INF008-Codes/Lista 3/l3q3/src/main/java/br/ifba/l3q3/paymentProcessor/IPaymentProcessor.java
+++ INF008-Codes/Lista 3/l3q3/src/main/java/br/ifba/l3q3/paymentProcessor/IPaymentProcessor.java
@@ -7,4 +7,7 @@ public interface IPaymentProcessor {
     
     // Processes the payment and returns true if is ok
     public boolean processor(IPayment payment, boolean riskAnalyzed);
+
+    // Implementar futuramente
+    //Class<? extends IPayment> getAcceptedPaymentType();
 }
