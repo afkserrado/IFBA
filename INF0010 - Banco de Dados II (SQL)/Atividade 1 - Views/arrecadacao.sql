62066804796bc0f76a0e79db8f540d240bd5aa0e
afkserrado <akserrado@gmail.com>
2026-04-02 19:27:23 -0300

    Formatação do arrecadacao.sql

--- INF0010 - Banco de Dados II (SQL)/Atividade 1 - Views/arrecadacao.sql
+++ INF0010 - Banco de Dados II (SQL)/Atividade 1 - Views/arrecadacao.sql
@@ -1,12 +1,20 @@
 -- Mostrar a quantidade de passageiros e a receita arrecadada por linha em uma data
 
-CREATE MATERIALIZED VIEW Arrecadacao
-AS 	SELECT L.numero, L.descricao, V.data_entrada AS data_viagem, COUNT(V.id_viagem) AS passageiros, SUM(V.tarifa) AS receita
-	FROM Linha L 
-	JOIN Viagem V
-		ON L.numero = V.num_linha
-	GROUP BY L.numero, L.descricao, V.data_entrada
-	ORDER BY V.data_entrada, L.numero;
+DROP MATERIALIZED VIEW IF EXISTS Arrecadacao;
 
+CREATE MATERIALIZED VIEW Arrecadacao AS
+SELECT
+	L.numero,
+	L.descricao,
+	V.data_entrada AS data_viagem,
+	COUNT(V.id_viagem) AS passageiros,
+	SUM(V.tarifa) AS receita
+
+FROM Linha L
+JOIN Viagem V
+	ON L.numero = V.num_linha
+GROUP BY L.numero, L.descricao, V.data_entrada
+ORDER BY V.data_entrada, L.numero;
+
+-- REFRESH MATERIALIZED VIEW Arrecadacao;
 --SELECT * FROM Arrecadacao;
-
