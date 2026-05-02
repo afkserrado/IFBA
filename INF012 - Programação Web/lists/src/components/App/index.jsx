9859e47280b335a84ea36679bc25e54516da222e
afkserrado <akserrado@gmail.com>
2026-05-02 17:05:34 -0300

    First commit: lists

---
+++ INF012 - Programação Web/lists/src/components/App/index.jsx
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+import Feed from '../Feed';
+
+class App extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            feed: [
+                {id: 1, nome: 'Manoel', curtidas: 34, comentarios: 23},
+                {id:2, nome:'Maria', curtidas:45, comentarios:12},
+                {id:3, nome:'Joao', curtidas:102, comentarios:70},
+            ]
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                {this.state.feed.map(item => {
+                    return(
+                        <Feed 
+                            key={item.id}
+                            id={item.id} 
+                            nome={item.nome} 
+                            curtidas={item.curtidas} 
+                            comentarios={item.comentarios}
+                        />
+                    );
+                })}
+            </div>
+        );
+    }
+} export default App;
