98ef6c4ded0cd10e0a11a979b628a5726c670b8c
afkserrado <akserrado@gmail.com>
2026-05-02 18:11:36 -0300

    First commit: biscoito da sorte

---
+++ INF012 - Programação Web/biscoito/src/components/App/index.jsx
@@ -0,0 +1,39 @@
+import { Component } from 'react';
+
+import '../../App.css';
+
+import biscoito from '../../assets/biscoito.jpg';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            fraseSorteada: "",
+        };
+
+        this.frases = [
+            "A vida trará coisas boas se tiveres paciência.",
+            "Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você.",
+            "Não compense na ira o que lhe falta na razão.",
+            "Defeitos e virtudes são apenas dois lados da mesma moeda.",
+            "A maior de todas as torres começa no solo.",
+            "Não há que ser forte.",
+        ]
+    }
+
+    abrirBiscoito() {
+        let numero = Math.floor(Math.random() * this.frases.length);
+        this.setState({ fraseSorteada: this.frases[numero] });
+    }
+
+    render() {
+        return(
+            <div className="conteiner">
+                <img alt="biscoito" src={biscoito} className="img"/>
+                <button onClick={this.abrirBiscoito.bind(this)}>Sortear</button>
+                <h1 className="textoFrase">{this.state.fraseSorteada}</h1>
+            </div>
+        )
+    }
+} export default App;
