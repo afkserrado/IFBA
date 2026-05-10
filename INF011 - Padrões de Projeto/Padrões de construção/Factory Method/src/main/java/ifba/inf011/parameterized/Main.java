febc7d5f494190635270ddec328c4ea60d9b16f7
afkserrado <akserrado@gmail.com>
2026-05-10 19:00:02 -0300

    Ajustes nos mains

--- INF011 - Padrões de Projeto/Padrões de construção/Factory Method/src/main/java/ifba/inf011/parameterized/Main.java
+++ INF011 - Padrões de Projeto/Padrões de construção/Factory Method/src/main/java/ifba/inf011/parameterized/Main.java
@@ -3,9 +3,11 @@ package ifba.inf011.parameterized;
 import ifba.inf011.parameterized.creators.Creator;
 import ifba.inf011.parameterized.enums.ProductId;
 
+// Cliente
 public class Main {
-    public static void main(String[] args) {
+    public static void run() {
         Creator creator = new Creator();
+
         creator.anOperation(ProductId.A);
         creator.anOperation(ProductId.B);
     }
