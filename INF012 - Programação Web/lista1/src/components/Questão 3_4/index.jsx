fd84e3df4380f168230130ea3051049378798caa
Anderson Serrado <akserrado@gmail.com>
2026-05-14 01:49:57 -0300

    Questão 2

---
+++ INF012 - Programação Web/lista1/src/components/Questão 3_4/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Q3_4 extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Questões 3 e 4</h1>
+            </div>
+        );
+    }
+}
+
+export default Q3_4;
--- INF012 - Programação Web/lista1/src/pages/Questao2/index.jsx
+++
@@ -1,7 +0,0 @@
-
-
-function Q2() {
-    return(
-        <h1>Questão 2</h1>
-    );
-} export default Q2;
