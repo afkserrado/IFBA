d14cc801ab8759556e9fe8f313a9cb22be2efeda
afkserrado <akserrado@gmail.com>
2026-05-24 20:23:21 -0300

    Att

--- INF010 - Banco de Dados II (SQL)/Atividade 6 - Otimização de consultas/query.sql
+++ INF010 - Banco de Dados II (SQL)/Atividade 6 - Otimização de consultas/query.sql
@@ -87,6 +87,10 @@
 -- );
 
 -- Abordagem com ORDER BY + LIMIT
+-- CREATE INDEX idx_indice_longevidade 
+-- ON Indice(IDH_Longevidade DESC);
+-- DROP INDEX idx_indice_longevidade;
+
 -- SELECT M.CodMunicipio, TRIM(M.NomeMunicipio) AS NomeMunicipio, I.IDH_Longevidade	
 -- FROM Indice I
 -- JOIN Municipio M
@@ -95,16 +99,79 @@
 -- LIMIT 1;
 
 -- 6 - Ano em que Salvador obteve o melhor índice de Instrução
+-- CREATE INDEX idx_indice_municipio_educacao
+-- ON Indice(CodMunicipio, IDH_Educacao DESC);
 
+-- DROP INDEX idx_indice_municipio_educacao;
+
+-- Abordagem com JOIN
+-- SELECT I.Ano, I.IDH_Educacao
+-- FROM Indice I
+-- JOIN Municipio M
+-- 	ON I.CodMunicipio = M.CodMunicipio
+-- WHERE M.NomeMunicipio = 'Salvador'
+-- ORDER BY I.IDH_Educacao DESC
+-- LIMIT 1;
+
+-- Abordagem com EXISTS
+-- SELECT I.Ano, I.IDH_Educacao
+-- FROM Indice I
+-- WHERE EXISTS (
+-- 	SELECT *
+-- 	FROM Municipio M
+-- 	WHERE I.CodMunicipio = M.CodMunicipio AND M.NomeMunicipio = 'Salvador'
+-- )
+-- ORDER BY I.IDH_Educacao DESC
+-- LIMIT 1;
 
 -- 7 - Qual o município com a melhor distribuição de renda
+-- CREATE INDEX idx_indice_renda
+-- ON Indice(IDH_Renda DESC);
 
+-- DROP INDEX idx_indice_renda;
+
+-- SELECT M.NomeMunicipio, E.NomeEstado, I.Ano, I.IDH_Renda
+-- FROM Indice I
+-- JOIN Municipio M
+-- 	ON I.CodMunicipio = M.CodMunicipio
+-- JOIN Estado E
+-- 	ON M.CodEstado = E.CodEstado
+-- ORDER BY I.IDH_Renda DESC
+-- LIMIT 1;
 
 -- 8 - Quais estados possuem municípios com IDH geral maior que 0,8
+-- CREATE INDEX idx_indice_idh_geral
+-- ON Indice (IDH_Geral, CodMunicipio);
+-- DROP INDEX idx_indice_idh_geral;
 
+-- Abordagem com JOIN
+-- SELECT DISTINCT(E.NomeEstado)
+-- FROM Estado E
+-- JOIN Municipio M
+-- 	ON E.CodEstado = M.CodEstado
+-- JOIN Indice I
+-- 	ON M.CodMunicipio = I.CodMunicipio
+-- WHERE I.IDH_Geral > 0.8;
+
+-- Abordagem com EXISTS
+-- SELECT DISTINCT(E.NomeEstado)
+-- FROM Estado E
+-- JOIN Municipio M
+-- 	ON E.CodEstado = M.CodEstado
+-- WHERE EXISTS (
+-- 	SELECT *
+-- 	FROM Indice I
+-- 	WHERE M.CodMunicipio = I.CodMunicipio AND I.IDH_Geral > 0.8
+-- );
 
 -- 9 - Qual o maior IDH de educação por estado
-
+-- SELECT E.NomeEstado, MAX(I.IDH_Educacao)
+-- FROM Estado E
+-- JOIN Municipio M
+-- 	ON E.CodEstado = M.CodEstado
+-- JOIN Indice I
+-- 	ON M.CodMunicipio = I.CodMunicipio
+-- GROUP BY E.NomeEstado;
 
 -- 10- Relatório de Todos IDHs da Bahia de 91 e 2000, inclusive com a
 -- diferença entre os mesmos
@@ -226,27 +293,27 @@
 
 -- 11 - Relatório comparativo entre as médias dos IDHs de SC e AL,
 -- de 2000 e 91
-SELECT E.SiglaEstado,
+-- SELECT E.SiglaEstado,
 
-	AVG(CASE WHEN I.Ano = 1991 THEN I.IDH_Geral END) AS IDH_Geral_1991,
-	AVG(CASE WHEN I.Ano = 2000 THEN I.IDH_Geral END) AS IDH_Geral_2000,
+-- 	AVG(CASE WHEN I.Ano = 1991 THEN I.IDH_Geral END) AS IDH_Geral_1991,
+-- 	AVG(CASE WHEN I.Ano = 2000 THEN I.IDH_Geral END) AS IDH_Geral_2000,
 
-	AVG(CASE WHEN I.Ano = 1991 THEN I.IDH_Renda END) AS IDH_Renda_1991,
-	AVG(CASE WHEN I.Ano = 2000 THEN I.IDH_Renda END) AS IDH_Renda_2000,
+-- 	AVG(CASE WHEN I.Ano = 1991 THEN I.IDH_Renda END) AS IDH_Renda_1991,
+-- 	AVG(CASE WHEN I.Ano = 2000 THEN I.IDH_Renda END) AS IDH_Renda_2000,
 	
-	AVG(CASE WHEN I.Ano = 1991 THEN I.IDH_Longevidade END) AS IDH_Longevidade_1991,
-	AVG(CASE WHEN I.Ano = 2000 THEN I.IDH_Longevidade END) AS IDH_Longevidade_2000,
+-- 	AVG(CASE WHEN I.Ano = 1991 THEN I.IDH_Longevidade END) AS IDH_Longevidade_1991,
+-- 	AVG(CASE WHEN I.Ano = 2000 THEN I.IDH_Longevidade END) AS IDH_Longevidade_2000,
 	
-	AVG(CASE WHEN I.Ano = 1991 THEN I.IDH_Educacao END) AS IDH_Educacao_1991,
-	AVG(CASE WHEN I.Ano = 2000 THEN I.IDH_Educacao END) AS IDH_Educacao_2000
+-- 	AVG(CASE WHEN I.Ano = 1991 THEN I.IDH_Educacao END) AS IDH_Educacao_1991,
+-- 	AVG(CASE WHEN I.Ano = 2000 THEN I.IDH_Educacao END) AS IDH_Educacao_2000
 
-FROM Indice I
+-- FROM Indice I
 
-JOIN Municipio M
-	ON I.CodMunicipio = M.CodMunicipio
+-- JOIN Municipio M
+-- 	ON I.CodMunicipio = M.CodMunicipio
 
-JOIN Estado E
-	ON M.CodEstado = E.CodEstado
+-- JOIN Estado E
+-- 	ON M.CodEstado = E.CodEstado
 
-WHERE E.SiglaEstado IN ('SC', 'AL')
-GROUP BY E.SiglaEstado;
+-- WHERE E.SiglaEstado IN ('SC', 'AL')
+-- GROUP BY E.SiglaEstado;
