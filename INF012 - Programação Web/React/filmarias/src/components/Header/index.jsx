1eb0c67317599419cee116781fe4127943bc3598
Anderson Serrado <akserrado@gmail.com>
2026-05-28 14:50:23 -0300

    Atualização da estrutura de pastas

---
+++ INF012 - Programação Web/React/filmarias/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+export default function Header() {
+    return(
+        <header>
+            <Link className="logo" to="/">Filmaria</Link>
+            <Link className="favoritos" to="/favoritos">Salvos</Link>
+        </header>
+    );
+}
--- INF012 - Programação Web/lista1/src/services/api/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// API para fazer requisições ao banco de questões da lista
-const api = axios.create({
-    baseURL: 'http://localhost:3000/questoes'
-});
-
-export default api;
