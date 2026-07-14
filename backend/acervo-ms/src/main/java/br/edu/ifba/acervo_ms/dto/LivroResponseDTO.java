6337bd5f89db8f1df8fece86d3cb9b3a5b6c78ba
afkserrado <akserrado@gmail.com>
2026-07-14 12:15:10 -0300

    Criação dos dtos de livro

--- backend/acervo-ms/src/main/java/br/edu/ifba/acervo_ms/dto/LivroResponseDTO.java
+++ backend/acervo-ms/src/main/java/br/edu/ifba/acervo_ms/dto/LivroResponseDTO.java
@@ -1,65 +1,20 @@
-package br.edu.ifba.acervo_ms.entity;
+package br.edu.ifba.acervo_ms.dto;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+// DTO para retorno de dados de livro nas respostas da API
+public class LivroResponseDTO {
 
-// Representa a entidade "livros" no banco de dados
-@Entity
-@Table(name = "livros")
-public class Livro {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
     private String titulo;
-
-    @Column(nullable = false)
     private String autor;
-
-    @Column(
-        unique = true,
-        nullable = false
-    )
     private String isbn;
-
-    @Column(nullable = false)
     private Integer quantidadeTotal;
-
-    @Column(nullable = false)
     private Integer quantidadeDisponivel;
-
-    @Column(
-        nullable = false,
-        updatable = false
-    )
     private LocalDateTime dataCriacao;
-
-    @Column(nullable = false)
     private LocalDateTime dataAtualizacao;
 
-    // Construtor utilizado pelo JPA/Hibernate para reconstruir
-    // a partir dos dados do banco
-    public Livro() {}
-
-    // Construtor utilizado pelo service correspondente
-    // para construir um objeto a partir dos dados de um dto
-    public Livro(String titulo, String autor, String isbn, Integer quantidadeTotal) {
-        this.titulo = titulo;
-        this.autor = autor;
-        this.isbn = isbn;
-        this.quantidadeTotal = quantidadeTotal;
-        this.quantidadeDisponivel = quantidadeTotal;
-        this.dataCriacao = LocalDateTime.now();
-        this.dataAtualizacao = LocalDateTime.now();
-    }
+    public LivroResponseDTO() {}
 
     public Long getId() {
         return id;
