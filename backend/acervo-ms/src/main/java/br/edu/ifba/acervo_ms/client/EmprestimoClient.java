b9944ea0f80dd7d6be047c1e69d82a245cf74578
afkserrado <akserrado@gmail.com>
2026-07-14 23:08:10 -0300

    feat(acervo): cria LivroService, ordenação de livros e integração com emprestimos-ms

---
+++ backend/acervo-ms/src/main/java/br/edu/ifba/acervo_ms/client/EmprestimoClient.java
@@ -0,0 +1,15 @@
+package br.edu.ifba.acervo_ms.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+    name = "emprestimos-ms", 
+    url = "${services.emprestimos.url}"
+)
+public interface EmprestimoClient {
+    
+    @GetMapping("/api/v1/emprestimos/livros/{livroId}/ativos/existe")
+    Boolean existeEmprestimoAtivoPorLivro(@PathVariable("livroId") Long livroId);
+}
--- backend/acervo-ms/src/main/java/br/edu/ifba/acervo_ms/service/LivroService.java
+++ backend/acervo-ms/src/main/java/br/edu/ifba/acervo_ms/service/LivroService.java
@@ -7,13 +7,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.edu.ifba.acervo_ms.client.EmprestimoClient;
 import br.edu.ifba.acervo_ms.dto.LivroRequestDTO;
-import br.edu.ifba.acervo_ms.dto.LivroResponseDTO;
 import br.edu.ifba.acervo_ms.dto.LivroResumoResponseDTO;
 import br.edu.ifba.acervo_ms.entity.Livro;
+import br.edu.ifba.acervo_ms.enums.OrdenacaoLivro;
+import br.edu.ifba.acervo_ms.exception.LivroNaoEncontradoException;
 import br.edu.ifba.acervo_ms.exception.OperacaoNaoPermitidaException;
 import br.edu.ifba.acervo_ms.mapper.LivroMapper;
 import br.edu.ifba.acervo_ms.repository.LivroRepository;
@@ -22,12 +24,15 @@ import br.edu.ifba.acervo_ms.repository.LivroRepository;
 public class LivroService {
     
     private final LivroRepository livroRepository;
+    private final EmprestimoClient emprestimoClient;
 
-    public LivroService(LivroRepository livroRepository) {
+    public LivroService(LivroRepository livroRepository, EmprestimoClient emprestimoClient) {
         this.livroRepository = livroRepository;
+        this.emprestimoClient = emprestimoClient;
     }
 
-    public LivroResponseDTO cadastrarLivro(LivroRequestDTO dto) {
+    @Transactional
+    public LivroResumoResponseDTO cadastrarLivro(LivroRequestDTO dto) {
 
         if(livroRepository.existsByIsbn(dto.getIsbn())) {
             throw new OperacaoNaoPermitidaException(
@@ -40,52 +45,154 @@ public class LivroService {
         );
 
         Livro livroSalvo = livroRepository.save(livro);
-        return new LivroResponseDTO(livroSalvo);
+        return LivroMapper.converterEntidadeParaDtoResumido(livroSalvo);
     }
 
-    // Adicionar split do sort
+    public List<LivroResumoResponseDTO> buscarLivros(String ordenacao) {
+        Sort sort = OrdenacaoLivro.resolverSort(ordenacao);
+        List<Livro> livros = livroRepository.findAll(sort);
+        return LivroMapper.converterEntidadesParaDtoResumido(livros);
+    }
 
-    // public List<LivroResumoResponseDTO> buscarLivros(String sort) {
+    public Page<LivroResumoResponseDTO> buscarLivros(String ordenacao, Pageable pageable) {
+        Pageable pageableComSort = criarPageableComOrdenacao(pageable, ordenacao);
+        Page<Livro> livros = livroRepository.findAll(pageableComSort);
+        return LivroMapper.converterEntidadesParaDtoResumido(livros);
+    }
 
-    //     List<Livro> livros;
-
-    //     if (sort == null || sort.isBlank()) {
-    //         livros = livroRepository.findAll();
-    //     }
+    public LivroResumoResponseDTO buscarLivroPorIsbn(String isbn) {
         
-    //     else if (sort.equals("titulo") || sort.equals("autor")) {
-    //         livros = livroRepository.findAll(Sort.by(sort));
-    //     }
+        Livro livro = livroRepository.findByIsbn(isbn)
+            .orElseThrow(LivroNaoEncontradoException::new);
+
+        return LivroMapper.converterEntidadeParaDtoResumido(livro);
+    }
+
+    public List<LivroResumoResponseDTO> buscarLivrosPorAutor(String autor, String ordenacao) {
+        Sort sort = OrdenacaoLivro.resolverSort(ordenacao);
+        List<Livro> livros = livroRepository.findByAutorContainingIgnoreCase(autor, sort);
+        return LivroMapper.converterEntidadesParaDtoResumido(livros);
+    }
+
+    public Page<LivroResumoResponseDTO> buscarLivrosPorAutor(String autor, String ordenacao, Pageable pageable) {
+        Pageable pageableComSort = criarPageableComOrdenacao(pageable, ordenacao);
+        Page<Livro> livros = livroRepository.findByAutorContainingIgnoreCase(autor, pageableComSort);
+        return LivroMapper.converterEntidadesParaDtoResumido(livros);
+    }
+
+    public List<LivroResumoResponseDTO> buscarLivrosPorTitulo(String titulo, String ordenacao) {
+        Sort sort = OrdenacaoLivro.resolverSort(ordenacao);
+        List<Livro> livros = livroRepository.findByTituloContainingIgnoreCase(titulo, sort);
+        return LivroMapper.converterEntidadesParaDtoResumido(livros);
+    }
+
+    public Page<LivroResumoResponseDTO> buscarLivrosPorTitulo(String titulo, String ordenacao, Pageable pageable) {
+        Pageable pageableComSort = criarPageableComOrdenacao(pageable, ordenacao);
+        Page<Livro> livros = livroRepository.findByTituloContainingIgnoreCase(titulo, pageableComSort);
+        return LivroMapper.converterEntidadesParaDtoResumido(livros);
+    }
+
+    @Transactional
+    public LivroResumoResponseDTO atualizarLivro(Long id, LivroRequestDTO dto) {
+
+        if(livroRepository.existsByIsbnAndIdNot(dto.getIsbn(), id)) {
+            throw new OperacaoNaoPermitidaException(
+            "Já existe um livro com esse ISBN");
+        }
+
+        Livro livro = obterLivro(id);
+
+        int emprestados = livro.getQuantidadeTotal() - livro.getQuantidadeDisponivel();
+
+        if(dto.getQuantidadeTotal() < emprestados) { 
+            throw new OperacaoNaoPermitidaException("A quantidade total não pode ser menor que a quantidade de exemplares emprestados.");
+        }
+
+        livro.setTitulo(dto.getTitulo());
+        livro.setAutor(dto.getAutor());
+        livro.setIsbn(dto.getIsbn());
+        livro.setQuantidadeTotal(dto.getQuantidadeTotal());
+        livro.setQuantidadeDisponivel(dto.getQuantidadeTotal() - emprestados);
+
+        return LivroMapper.converterEntidadeParaDtoResumido(livro);
+    }
+
+    @Transactional
+    public void removerLivro(Long id) {
         
-    //     else {
-    //         throw new OperacaoNaoPermitidaException("Ordenação permitida apenas por título ou autor.");
-    //     }
+        Livro livro = obterLivro(id);
 
-    //     return LivroMapper.converterEntidadesParaDtoResumido(livros);
-    // }
+        try {
 
-    // public Page<LivroResumoResponseDTO> buscarLivros(Pageable pageable, String sort) {
+            Boolean possuiEmprestimosAtivos = emprestimoClient.existeEmprestimoAtivoPorLivro(id);
 
-    //     Page<Livro> livros;
+            if(Boolean.TRUE.equals(possuiEmprestimosAtivos)) {
+                throw new OperacaoNaoPermitidaException(
+                "O livro não pode ser excluído, pois possui empréstimos ativos."
+                );  
+            }
+        } 
 
-    //     if (sort == null || sort.isBlank()) {
-    //         livros = livroRepository.findAll(pageable);
-    //     }
+        catch (OperacaoNaoPermitidaException ex) {
+            throw ex;
+        }
+
+        // Falha de conexão com emprestimos-ms
+        catch(Exception ex) {
+            throw new OperacaoNaoPermitidaException(
+            "Não foi possível validar os empréstimos ativos do livro no momento."
+            );
+        }
+
+        livroRepository.delete(livro);
+    }
+
+    public boolean estaDisponivel(Long id) {
+        Livro livro = obterLivro(id);
+        return livro.getQuantidadeDisponivel() > 0;
+    }
+
+    @Transactional
+    public void reduzirEstoque(Long id) {
         
-    //     else if (sort.equals("titulo") || sort.equals("autor")) {
-    //         Pageable pageableComSort = PageRequest.of(
-    //             pageable.getPageNumber(),
-    //             pageable.getPageSize(),
-    //             Sort.by(sort)
-    //         );
+        Livro livro = obterLivro(id);
 
-    //         livros = livroRepository.findAll(pageableComSort);
-    //     }
+        if (livro.getQuantidadeDisponivel() <= 0) {
+            throw new OperacaoNaoPermitidaException(
+                "O livro não possui exemplares disponíveis para empréstimo."
+            );
+        }
+
+        livro.setQuantidadeDisponivel(livro.getQuantidadeDisponivel() - 1);
+    }
+
+    @Transactional
+    public void aumentarEstoque(Long id) {
         
-    //     else {
-    //         throw new OperacaoNaoPermitidaException("Ordenação permitida apenas por título ou autor.");
-    //     }
+        Livro livro = obterLivro(id);
+        
+        if (livro.getQuantidadeDisponivel() >= livro.getQuantidadeTotal()) {
+            throw new OperacaoNaoPermitidaException(
+                "A quantidade disponível não pode ser maior que a quantidade total."
+            );
+        }
 
-    //     return LivroResponseDto.converterEntidadesParaDto(livros);
-    // }
+        livro.setQuantidadeDisponivel(livro.getQuantidadeDisponivel() + 1);
+    }
+
+    private Pageable criarPageableComOrdenacao(Pageable pageable, String ordenacao) {
+        
+        Sort sort = OrdenacaoLivro.resolverSort(ordenacao);
+
+        return PageRequest.of(
+            pageable.getPageNumber(),
+            pageable.getPageSize(),
+            sort
+        );
+    }
+
+    private Livro obterLivro(Long id) {
+        return livroRepository.findById(id)
+            .orElseThrow(LivroNaoEncontradoException::new);
+    }
 }
