cc5a6f7d920abd704aaed3cbf26595736c00c8be
afkserrado <akserrado@gmail.com>
2026-07-20 10:18:29 -0300

    Remoção de anotação desnecessária

--- backend/emprestimos-ms/src/main/java/br/edu/ifba/emprestimos_ms/repository/EmprestimoRepository.java
+++ backend/emprestimos-ms/src/main/java/br/edu/ifba/emprestimos_ms/repository/EmprestimoRepository.java
@@ -3,14 +3,12 @@ package br.edu.ifba.emprestimos_ms.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import br.edu.ifba.emprestimos_ms.entity.Emprestimo;
 import br.edu.ifba.emprestimos_ms.entity.StatusEmprestimo;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Repository
 public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long> {
 
     // Regra 1: Conta quantos empréstimos ATIVOS o usuário possui
