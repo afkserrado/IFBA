e1e8cf05a5539f1280b89aae0e6d30dc39da4287
afkserrado <akserrado@gmail.com>
2026-07-20 11:55:37 -0300

    Correção de imports

--- backend/emprestimos-ms/src/main/java/br/edu/ifba/emprestimos_ms/controller/EmprestimoController.java
+++ backend/emprestimos-ms/src/main/java/br/edu/ifba/emprestimos_ms/controller/EmprestimoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.edu.ifba.emprestimos_ms.dto.EmprestimoRequest;
 import br.edu.ifba.emprestimos_ms.dto.EmprestimoResponse;
 import br.edu.ifba.emprestimos_ms.service.EmprestimoService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import jakarta.validation.Valid;
 
 @RestController
--- backend/emprestimos-ms/src/main/java/br/edu/ifba/emprestimos_ms/exception/GlobalExceptionHandler.java
+++ backend/emprestimos-ms/src/main/java/br/edu/ifba/emprestimos_ms/exception/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
