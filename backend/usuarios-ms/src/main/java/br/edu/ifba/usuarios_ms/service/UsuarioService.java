2d95b811d7441bd6ced9e162184affa2caeed500
afkserrado <akserrado@gmail.com>
2026-07-20 13:43:44 -0300

    Substituição das exceções lançadas para recurso não encontrado

--- backend/usuarios-ms/src/main/java/br/edu/ifba/usuarios_ms/service/UsuarioService.java
+++ backend/usuarios-ms/src/main/java/br/edu/ifba/usuarios_ms/service/UsuarioService.java
@@ -10,6 +10,7 @@ import br.edu.ifba.usuarios_ms.dto.UsuarioRequestDTO;
 import br.edu.ifba.usuarios_ms.dto.UsuarioResponseDTO;
 import br.edu.ifba.usuarios_ms.dto.UsuarioUpdateRequestDTO;
 import br.edu.ifba.usuarios_ms.entity.Usuario;
+import br.edu.ifba.usuarios_ms.exception.ResourceNotFoundException;
 import br.edu.ifba.usuarios_ms.messaging.UsuarioProducer;
 import br.edu.ifba.usuarios_ms.repository.UsuarioRepository;
 
@@ -88,7 +89,7 @@ public class UsuarioService {
         }
 
         Usuario usuario = usuarioRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Usuário não encontrado com o ID: " + id));
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado com o ID: " + id));
         return new UsuarioResponseDTO(usuario);
     }
 
@@ -115,7 +116,7 @@ public class UsuarioService {
         }
 
         Usuario usuarioAlvo = usuarioRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Usuário não encontrado com o ID: " + id));
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado com o ID: " + id));
 
         if (!usuarioAlvo.getEmail().equals(dto.email()) && usuarioRepository.existsByEmail(dto.email())) {
             throw new IllegalArgumentException("E-mail já está em uso por outro usuário.");
@@ -148,7 +149,7 @@ public class UsuarioService {
         }
 
         if (!usuarioRepository.existsById(id)) {
-            throw new RuntimeException("Usuário não encontrado com o ID: " + id);
+            throw new ResourceNotFoundException("Usuário não encontrado com o ID: " + id);
         }
 
         // Valida se ha pendencias em emprestimos antes de apagar
@@ -173,7 +174,7 @@ public class UsuarioService {
         }
 
         Usuario usuario = usuarioRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Usuário não encontrado com o e-mail: " + email));
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado com o e-mail: " + email));
         return new UsuarioResponseDTO(usuario);
     }
 }
