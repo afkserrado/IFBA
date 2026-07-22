49ad444d4db03b249b5d7f9a4818dbbd59ff6c02
afkserrado <akserrado@gmail.com>
2026-07-22 00:18:11 -0300

    Comentários

--- scripts/parar-ms.sh
+++ scripts/parar-ms.sh
@@ -4,9 +4,9 @@ kill $(lsof -t -i:8080 -i:8081 -i:8082 -i:8083 -i:8084 -i:8085)
 
 # Confirmar se foram interrompidos
 
-lsof -i :8080
-lsof -i :8081
-lsof -i :8082
-lsof -i :8083
-lsof -i :8084
-lsof -i :8085
+lsof -i :8080 # gateway-ms
+lsof -i :8081 # usuarios-ms
+lsof -i :8082 # acervo-ms
+lsof -i :8083 # emprestimos-ms
+lsof -i :8084 # notificacoes-ms
+lsof -i :8085 # email-ms
